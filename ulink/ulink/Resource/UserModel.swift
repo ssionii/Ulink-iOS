c8620e8e1141ab95500365dd83c5cf0b11a1e0fc
i-colours-u <i.colours.u@gmail.com>
2020-07-01 04:13:59 +0900

    채팅 viewcontroller 완성

---
+++ ulink/ulink/Resource/UserModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserModel.swift
+//  ulink
+//
+//  Created by 송지훈 on 2020/06/30.
+//  Copyright © 2020 송지훈. All rights reserved.
+//
+
+import Foundation
---
+++ ulink/ulink/Sources/ViewControllers/chatting/ChattingRoomViewController.swift
@@ -0,0 +1,30 @@
+//
+//  chattingRoomViewController.swift
+//  ulink
+//
+//  Created by 송지훈 on 2020/07/01.
+//  Copyright © 2020 송지훈. All rights reserved.
+//
+
+import UIKit
+
+class chattingRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
