ef98701cc80215f2a75e074e956c6b3886d9e6be
i-colours-u <i.colours.u@gmail.com>
2020-07-01 17:49:09 +0900

    firebase 유저 가입 및 로그인 완료

---
+++ ulink/ulink/Resource/API/APIConstants.swift
@@ -0,0 +1,9 @@
+//
+//  APIConstants.swift
+//  ulink
+//
+//  Created by 송지훈 on 2020/07/01.
+//  Copyright © 2020 송지훈. All rights reserved.
+//
+
+import Foundation
---
+++ ulink/ulink/Resource/Model/UserModel.swift
@@ -0,0 +1,19 @@
+//
+//  UserModel.swift
+//  ulink
+//
+//  Created by 송지훈 on 2020/06/30.
+//  Copyright © 2020 송지훈. All rights reserved.
+//
+
+import Foundation
+
+
+
+class UserModel : NSObject {
+
+    var profileImageUrl : String?
+    var userName : String?
+    var uid : String?
+    
+}
