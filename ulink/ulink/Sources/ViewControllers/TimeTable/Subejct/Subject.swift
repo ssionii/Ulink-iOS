35593829ca3b806b806fe6e13b609a7768888fe2
ssionii <ssionii353@gmail.com>
2020-07-02 12:37:03 +0900

    시간표 기본 틀 완성

---
+++ ulink/ulink/Sources/ViewControllers/TimeTable/Subejct/Subject.swift
@@ -0,0 +1,40 @@
+//
+//  Subject.swift
+//  ulink
+//
+//  Created by 양시연 on 2020/07/01.
+//  Copyright © 2020 송지훈. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public struct Subject {
+    public let subjectName : String
+    public let roomName : String
+    public let subjectDay : TimeTableDay
+    public let startTime : String
+    public let endTime : String
+    public let textColor : UIColor?
+    public let backgroundColor : UIColor
+    
+    public init(subjectName: String, roomName: String, subjectDay: TimeTableDay, startTime: String, endTime: String, textColor: UIColor?, backgroundColor: UIColor){
+        self.subjectName = subjectName
+        self.roomName = roomName
+        self.subjectDay = subjectDay
+        self.startTime = startTime
+        self.endTime = endTime
+        self.textColor = textColor
+        self.backgroundColor = backgroundColor
+    }
+    
+    public init(subjectName: String, roomName: String, subjectDay: TimeTableDay, startTime: String, endTime: String, backgroundColor: UIColor){
+        self.subjectName = subjectName
+        self.roomName = roomName
+        self.subjectDay = subjectDay
+        self.startTime = startTime
+        self.endTime = endTime
+        self.textColor = UIColor.white
+        self.backgroundColor = backgroundColor
+    }
+}
