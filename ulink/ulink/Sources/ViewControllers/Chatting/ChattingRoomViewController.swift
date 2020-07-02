0049299a779fda9e9dff6b7927fc5ec089167970
i-colours-u <i.colours.u@gmail.com>
2020-07-02 18:15:33 +0900

    성은 캘린더 추가

--- ulink/ulink/Sources/ViewControllers/Chatting/ChattingRoomViewController.swift
+++ ulink/ulink/Sources/ViewControllers/Chatting/ChattingRoomViewController.swift
@@ -1,18 +1,47 @@
 import UIKit
+import Firebase
+
 
 class ChattingRoomViewController: UIViewController {
 
     
+    @IBOutlet weak var sendButton: UIButton!
     
     
     public var destinationUid : String? // 나중에 내가 채팅할 대상의 uid
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
+        
+        
+        sendButton.addTarget(self,action:#selector(createRoom), for:.touchUpInside)
+        hideBar()
 
         // Do any additional setup after loading the view.
     }
     
 
+    
+    
+    func hideBar(){
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationController?.navigationBar.isHidden = true
+        
+    }
+    
+    
+    @objc func createRoom(){
+        
+        let createRoomInfo = [
+            "uid":Auth.auth().currentUser?.uid,
+            "destinationUid" : destinationUid
+        ]
+        
+        Database.database().reference().child("chatrooms").childByAutoId().setValue(createRoomInfo)
+        
+        
+    }
     /*
     // MARK: - Navigation
 
