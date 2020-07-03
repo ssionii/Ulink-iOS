779fab70df0ce652c8d92d3aaaf8da49eb35daf5
i-colours-u <i.colours.u@gmail.com>
2020-07-03 14:09:35 +0900

    채팅방 uid 생성

---
+++ ulink/ulink/Resource/API/APIConstant 2.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct APIConstants {
+     
+    static let baseURL = "http://13.209.144.115:3333"
+    static let signinURL = APIConstants.baseURL + "/user/signin"
+    static let signupURL = APIConstants.baseURL + "/user/signup"
+}
--- ulink/ulink/Sources/ViewControllers/Chatting/ChattingRoomViewController.swift
+++ ulink/ulink/Sources/ViewControllers/Chatting/ChattingRoomViewController.swift
@@ -1,5 +1,8 @@
 import UIKit
 import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
 
 
 class ChattingRoomViewController: UIViewController {
@@ -7,13 +10,17 @@ class ChattingRoomViewController: UIViewController {
     
     @IBOutlet weak var sendButton: UIButton!
     
+    var uid : String?
+    var chatRoomUid : String?
     
+    
+    @IBOutlet weak var messageTextField: UITextField!
     public var destinationUid : String? // 나중에 내가 채팅할 대상의 uid
     override func viewDidLoad() {
         
         
         super.viewDidLoad()
-        
+        uid = Auth.auth().currentUser?.uid
         
         sendButton.addTarget(self,action:#selector(createRoom), for:.touchUpInside)
         hideBar()
@@ -22,25 +29,56 @@ class ChattingRoomViewController: UIViewController {
     }
     
 
+    @IBAction func backToChattingList(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
     
     
     func hideBar(){
         self.tabBarController?.tabBar.isHidden = true
         self.navigationController?.navigationBar.isHidden = true
+
+
         
     }
     
     
     @objc func createRoom(){
         
-        let createRoomInfo = [
-            "uid":Auth.auth().currentUser?.uid,
-            "destinationUid" : destinationUid
+
+        let createRoomInfo :Dictionary<String,Any> = [ "users" :[
+            uid!: true,
+            destinationUid! : true
+            ]
         ]
         
-        Database.database().reference().child("chatrooms").childByAutoId().setValue(createRoomInfo)
         
+        if(chatRoomUid == nil){
+            // 방 생성 코드
+            Database.database().reference().child("chatrooms").childByAutoId().setValue(createRoomInfo)
+        }else{
+
+            
+            let value: Dictionary<String,Any> = [
+                "comments":[
+                    "uid": true,
+                    "message": messageTextField.text!
+                ]
+            ]
+
+            Database.database().reference().child("chatrooms").child(chatRoomUid!).child("comments").childByAutoId().setValue(value)
+        }
+    }
         
+
+    
+    func checkChatRoom() {
+        Database.database().reference().child("chatrooms").queryOrdered(byChild: "users/"+uid!).queryEqual(toValue: true).observeSingleEvent(of: DataEventType.value)
+        { (datasnapshot) in
+            for item in datasnapshot.children.allObjects as! [DataSnapshot]{
+                self.chatRoomUid = item.key
+            }
+        }
     }
     /*
     // MARK: - Navigation
