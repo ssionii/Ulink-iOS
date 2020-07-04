fdc7026191d661421d3f7dccd2e9dcd50ed45b8e
i-colours-u <i.colours.u@gmail.com>
2020-07-05 01:14:52 +0900

    notice ViewController 틀까지만..

--- ulink/ulink/Sources/ViewControllers/Chatting/rightSideMenuViewController.swift
+++ ulink/ulink/Sources/ViewControllers/Chatting/rightSideMenuViewController.swift
@@ -12,11 +12,23 @@ import SideMenu
 
 class rightSideMenuViewController: UIViewController {
 
+    @IBOutlet weak var noticeBlock: UIView!
+    @IBOutlet weak var photoBlock: UIView!
+    
+    @IBOutlet weak var QnABlock: UIView!
+    @IBOutlet weak var fileBlock: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         hideNavigationBar()
+        
+        
+        addGesture()
+        
 
-        // Do any additional setup after loading the view.
+
     }
     
     
@@ -26,24 +38,33 @@ class rightSideMenuViewController: UIViewController {
         self.navigationController?.navigationBar.isHidden = true
     }
     
+    
+    func addGesture(){
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(goToNoticePage(sender:)))
+        self.noticeBlock.addGestureRecognizer(gesture)
+        
+        
+    }
+    
+    
+    @objc func goToNoticePage(sender:UIGestureRecognizer)
+    {
+        let storyboard = UIStoryboard(name:"Chatting", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "NoticeViewController")
+        
+        
+        self.navigationController!.pushViewController(vc, animated: true)
+    }
+    
+    
+    
+    
      override var preferredStatusBarStyle: UIStatusBarStyle {
 
         return .default
 
-        }
-
-
-
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
